80b7a274b9d5405dd24a48e7d0585b18ff34c425
Valerius-Astoria <valeriusastoria@gmail.com>
2026-08-03 17:52:44 +0800

    add Faith.java and Country.java

--- src/main/java/com/valerius/namegenerator/Model/Country.java
+++ src/main/java/com/valerius/namegenerator/Model/Country.java
@@ -1,4 +1,15 @@
 package com.valerius.namegenerator.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Country {
+
+    @Id
+    private String id;
+    private String code;
+    private String name;
 }
--- src/main/java/com/valerius/namegenerator/Model/Faith.java
+++ src/main/java/com/valerius/namegenerator/Model/Faith.java
@@ -1,4 +1,18 @@
 package com.valerius.namegenerator.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
 public class Faith {
+
+    @Id
+    private String id;
+    private String code;
+    private String name;
+    @ManyToOne
+    private Faith parent;
 }
